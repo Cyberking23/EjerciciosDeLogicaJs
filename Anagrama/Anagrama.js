7f0a4ab8baaf351f8491152b4b57d010d88fa823
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-12-19 12:34:11 -0600

    Ejercicio de Anagrama y numeroPrimo
    Ejercicio de Anagrama y numeroPrimo

--- Anagrama/Anagrama.js
+++ Anagrama/Anagrama.js
@@ -1,36 +1,39 @@
-function sonAnagramas(palabra1, palabra2) {
-    // Paso 1: Comparar longitudes
-    if (palabra1.length !== palabra2.length) {
-        return false;
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+function Anagrama(palabra1, palabra2) {
+  if (palabra1.length !== palabra2.length) {
+    console.log("No es un anagrama");
+  }
+  if (palabra1 === palabra2) {
+    console.log("No es un anagrama");
+  }
+
+  let contador1 = {};
+  for (let letra of palabra1) {
+    contador1[letra] = (contador1[letra] || 0) + 1;
+  }
+
+  let contador2 = {};
+  for (let letra of palabra2) {
+    contador2[letra] = (contador2[letra] || 0) + 1;
+  }
+
+  for (let letra in contador1) {
+    if (contador1[letra] !== contador2[letra]) {
+      console.log("No es un anagrama");
     }
+  }
 
-    // Paso 2: Verificar si son exactamente iguales
-    if (palabra1 === palabra2) {
-        return false;
-    }
-
-    // Paso 3: Contar la frecuencia de cada letra
-    let contador1 = {};
-    let contador2 = {};
-
-    for (let letra of palabra1) {
-        contador1[letra] = (contador1[letra] || 0) + 1;
-    }
-
-    for (let letra of palabra2) {
-        contador2[letra] = (contador2[letra] || 0) + 1;
-    }
-
-    // Paso 4: Comparar frecuencias
-    for (let letra in contador1) {
-        if (contador1[letra] !== contador2[letra]) {
-            return false;
-        }
-    }
-
-    return true;
+  console.log(contador1);
+  console.log(contador2);
+  console.log("Es un anagrama");
 }
 
-// Ejemplo de uso
-console.log(sonAnagramas("amor", "roma")); // Debería retornar true
-console.log(sonAnagramas("casa", "saco")); // Debería retornar false
+console.log(Anagrama("amor", "roma"));
